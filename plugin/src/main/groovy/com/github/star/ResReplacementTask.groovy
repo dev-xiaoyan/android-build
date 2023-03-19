f66c8146bb48f3e9b767fe55bce51f9b81ceced4
dev-xiaoyan <92670658+dev-xiaoyan@users.noreply.github.com>
2023-03-19 14:19:37 +0400

    add raw support

--- plugin/src/main/groovy/com/github/star/ResReplacementTask.groovy
+++ plugin/src/main/groovy/com/github/star/ResReplacementTask.groovy
@@ -31,43 +31,31 @@ abstract class ResReplacementTask extends DefaultTask {
             }
             println("开始下载资源文件:${replacement.value}")
             InputStream byteStream = service.download(replacement.value).body().byteStream()
+            def fileExt = ""
+            def outputFile = ""
             switch (replacement.resType) {
                 case "image": {
-                    def outputFile = "${file.path}.webp"
-                    println("资源文件保存地址为:${outputFile}")
+                    fileExt = "webp"
+                    outputFile = "${file.path}.${fileExt}"
                     service.webp(byteStream, outputFile, replacement.width, replacement.height)
-                    def fileTree = project.fileTree(dir)
-                    fileTree.matching {
-                        include("${replacement.name}.*")
-                        exclude("${replacement.name}.webp")
-                    }.each {
-                        it.delete()
-                        println("正在删除冲突资源文件:${it.path}")
-                    }
                 }
                     break
                 case "raw": {
-                    def fileExt = replacement.ext.trim()
-                    String outputFile = ""
-                    if (fileExt == "" || fileExt == "*") {
-                        println("对于Raw类型的资源替换,无法推断出资源类型,尝试匹配中,如无法匹配,则替换失败")
-                        def fileTree = project.fileTree(dir)
-                        def matchedFiles = fileTree.matching {
-                            include("${replacement.name}.*")
-                        }
-                        if (matchedFiles.files.size() > 0) {
-                            println("匹配到可用资源类型:${matchedFiles.first().path}")
-                            outputFile = matchedFiles.first().path
-                        }
-                    } else {
-                        outputFile = project.file("$baseDir${replacement.name}.${fileExt}").path
-                    }
-                    println("资源文件保存地址为:${outputFile}")
+                    fileExt = "mp4"
+                    outputFile = "${file.path}.${fileExt}"
                     service.saveFile(byteStream, outputFile)
                 }
                     break
             }
-
+            println("资源文件保存地址为:${outputFile}")
+            def fileTree = project.fileTree(dir)
+            fileTree.matching {
+                include("${replacement.name}.*")
+                exclude("${replacement.name}.${fileExt}")
+            }.each {
+                it.delete()
+                println("正在删除冲突资源文件:${it.path}")
+            }
         }
     }
 }
